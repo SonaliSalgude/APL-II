4e9a0c17ad8606d59bcd5fd7ac25e8c8988eca46
SonaliSalgude <141991930+SonaliSalgude@users.noreply.github.com>
2024-11-11 13:17:26 +0530

    Add files via upload

--- buffer.js
+++ buffer.js
@@ -1,20 +1,2 @@
-const fs = require('fs');
-
-const data = 'Hello, Node.js with Buffer!';
-const buffer = Buffer.from(data, 'utf-8');
-
-fs.writeFile('example.txt', buffer, (err) => {
-  if (err) {
-    console.error('Error writing to file', err);
-  } else {
-    console.log('File written successfully!');
-
-    fs.readFile('example.txt', (err, data) => {
-      if (err) {
-        console.error('Error reading the file', err);
-      } else {
-        console.log('File content:', data.toString('utf-8'));
-      }
-    });
-  }
-});
+const buf = Buffer.from('Hello Buffer');
+console.log(buf.toString());
